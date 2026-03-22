3e7d13f8583856921e353e538dcf83a6b5c4ae1f
liyufeng.5 <liyufeng.5@bytedance.com>
2026-03-22 08:09:51 +0800

    add some file for docker

---
+++ assert/configurator_dumpenv.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash -
+
+xargs -0 bash -c 'printf "%q\n" "$@" ; systemctl set-environment "$@"' -- \
+        < /proc/1/environ \
+        > /var/docker_environment
+
+chmod 700 /var/docker_environment
